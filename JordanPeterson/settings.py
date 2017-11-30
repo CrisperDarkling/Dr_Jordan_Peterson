54cf4b5aec01420e2b0e43a2ae68cede29829d11
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-11-30 22:51:07 +0000

    14th commit 30/11 22:49 - added AWS to settings.py and added some media, $ Problem with media appearing on website and uploading  to AWS$. Also added icons to log-in etc

--- JordanPeterson/settings.py
+++ JordanPeterson/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "contact",
     "products",
     "search",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_S3_OBJECT_PARAMETERS = {  
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+
+AWS_STORAGE_BUCKET_NAME = 'jordan-peterson-bookstore'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
@@ -145,12 +158,12 @@ STATICFILES_DIRS = (
     )
 STATIC_URL = '/static/'
 STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# MEDIAFILES_LOCATION = 'media'
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")
