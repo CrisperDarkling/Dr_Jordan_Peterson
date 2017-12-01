26652d0f9ea4fcdc57b79706f1a8b725787d6cd5
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-12-01 17:17:17 +0000

    1/12 17:17 - Categories and products fully functional

--- categories/urls.py
+++ categories/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from .views import all_categories
+from products.views import products_by_category
 
 urlpatterns = [
     url(r'^$', all_categories, name='categories'),
+    url(r'^(\d+)$', products_by_category, name='products_by_category'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,3 +5,7 @@ from .models import Products
 def all_products(request):
     products = Products.objects.all()
     return render(request, "products2.html", {"products": products})
+    
+def products_by_category(request, id):
+    products = Products.objects.filter(category=id)
+    return render(request, "products2.html", {"products": products})
