77e1fa40eee1a9fafad054b2009b4e616337d5b9
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-12-01 04:33:03 +0000

    14.4 1/12 4:43 - still trying to fix this Travis Travestygit add .

--- JordanPeterson/settings.py
+++ JordanPeterson/settings.py
@@ -93,9 +93,9 @@ if "DATABASE_URL" in os.environ:
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
