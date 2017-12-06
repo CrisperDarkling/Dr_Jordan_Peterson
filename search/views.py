dbb2d03f2fb3769e492aeb706f70a57d5b696e0c
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-12-06 15:13:34 +0000

    23rd 6/12 15:12 - some CSS, and work on an improved Navbar and base.html (navConv.html)

--- search/views.py
+++ search/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from products.models import Products
+# from blog.models import Post
 
 # Create your views here.
 
 def do_search(request):
     products = Products.objects.filter(name__icontains=request.GET['q'])
-    return render(request, 'products.html', {'Products': products})
+    return render(request, 'products2.html', {'Products': products})
+    # return render(request, 'products2.html', {'Products': products}, "blogposts.html", {"Post": youtubeposts})
