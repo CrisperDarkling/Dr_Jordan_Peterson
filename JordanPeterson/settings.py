a3307f43f4eb51f9f14d82d92160b4a82e030383
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-12-14 10:40:48 +0000

    AWS

--- JordanPeterson/settings.py
+++ JordanPeterson/settings.py
@@ -187,37 +187,32 @@ STRIPE_SECRET = os.getenv("STRIPE_SECRET")
 
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
 
 
 
 
 
 
-# AWS_S3_OBJECT_PARAMETERS = {  
-#     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-#     'CacheControl': 'max-age=94608000',
-# }
+AWS_S3_OBJECT_PARAMETERS = {  
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
 
 
-# AWS_STORAGE_BUCKET_NAME = 'jordan-peterson-bookstore'
-# AWS_S3_REGION_NAME = 'eu-west-1'
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'jordan-peterson-bookstore'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-# MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
 
-# STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = "custom_storages.StaticStorage"
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
 
 
 
