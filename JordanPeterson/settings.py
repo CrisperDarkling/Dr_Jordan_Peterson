1132e29514c2b97718631819f1a9cb1f278ca8ed
Yazeed Kelly <yazeedkelly@hotmail.com>
2018-01-18 21:32:43 +0000

    trying to get book category images to CollectStatic to S3

--- JordanPeterson/settings.py
+++ JordanPeterson/settings.py
@@ -176,14 +176,19 @@ if USE_S3:
     AWS_STORAGE_BUCKET_NAME = 'jordan-peterson-bookstore'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     
     MEDIAFILES_LOCATION = 'media'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-
+    
     STATICFILES_LOCATION = 'static'
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+
     
     
 
