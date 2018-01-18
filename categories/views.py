34fbd7e3aa836b5e15affba1c64d29857bb5597d
Yazeed Kelly <yazeedkelly@hotmail.com>
2018-01-18 21:48:43 +0000

    54th 18/1 21:48 - final category images and ordering

--- categories/views.py
+++ categories/views.py
@@ -3,5 +3,5 @@ from .models import Category
 
 # Create your views here.
 def all_categories(request):
-    categories = Category.objects.all()
+    categories = Category.objects.all().order_by('name')
     return render(request, "categories.html", {"categories": categories})
